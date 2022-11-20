48bfd98f57aafb59f8d781aac03ebfbdb0cd7cc7
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-11-20 14:48:58 +0000

    Set up

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
 console.log("Hello world!");
+
+let doors = [];
+
+    
+
+    for (let i = 0; i < 3; i++) {
+       
+        doors[i] =  document.write('<img src="images/goat.jpg">');
+                    document.querySelector('.goat');
+
+
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+      
+      
+      let prize = getRandomInt(3);
+      prize.appendItem;
+
+
+});
+
+
+
+    
