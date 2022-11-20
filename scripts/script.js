075716797c4ca4bca1f7aab08de7bae17c511215
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-11-20 16:29:55 +0000

    Latest code

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 
-console.log("Hello world!");
-
-let doors = [];
-
-function setUp() {
-
-    for (let i = 0; i < 3; i++) {
-       
-        doors[i] =  document.write('<img src="images/goat.jpg">');
-                    document.querySelector('.goat');
-
-
-    }
-
-    let prize = document.write('<img src="images/sportscar.jpg">');
-    document.querySelector('.sportscar');
-
-        function getRandomInt(max) {
-            return Math.floor(Math.random() * max);
-    }
-      
+    console.log("Hello world!");
     
-    prize = getRandomInt(3);
-    console.log(prize);
+    let doors = [];
     
-    document.body.appendChild(prize);
-
-}
-
-setUp();
-
-});
-
-
-
+    function setUp() {
+    
+        for (let i = 0; i < 3; i++) {
+           
+            doors[i] =  document.write('<img src="images/goat.jpg">');
+                        document.querySelector('.goat');
+    
+    
+        }
+    
+        let prize = document.write('<img src="images/sportscar.jpg">');
+        document.querySelector('.sportscar');
+    
+            function getRandomInt(max) {
+                return Math.floor(Math.random() * max);
+        }
+          
+        
+        prize = getRandomInt(3);
+        console.log(prize);
+        
+        document.body.appendChild(prize);
+    
+    }
+    
+    setUp();
+    
+    });
     
