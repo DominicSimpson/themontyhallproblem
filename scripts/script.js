be3dfcaf5e2cf881994f3af076d690624e2e85d0
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-11-21 13:10:52 +0000

    Initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,32 @@
+// Declaring global variables
+const body = document.getElementById('body');
+const instructions = document.getElementById('instructions');
+const row1 = document.getElementById('row1');
+const row2 = document.getElementById('row2');
+const d1 = document.getElementById('d1');
+const d2 = document.getElementById('d2');
+const d3 = document.getElementById('d3');
+const switchChoiceYes = document.getElementById('btn-1');
+const switchChoiceNo = document.getElementById('btn-2');
+const doorImage1 = document.getElementById('door1');
+const doorImage2 = document.getElementById('door2');
+const doorImage3 = document.getElementById('door3');
+const SwitchAndWin = document.getElementById("switchAndWin");
+const SwitchAndLose = document.getElementById("switchAndLose");
+const NoSwitchAndWin = document.getElementById("NoSwitchAndWin");
+const NoSwitchAndLose = document.getElementById("NoSwitchAndLose");
+
+// Image of Car
+const winPath =
+"https://image.flaticon.com/icons/svg/3118/3118467.svg";
+// Image of Goat
+const losePath =
+"https://image.flaticon.com/icons/svg/836/836069.svg";
+
+// Variables for shuffling the doors
+var openDoor1, openDoor2, openDoor3, winner;
 
 
-    console.log("Hello world!");
-    
-    const doors = [];
-
-    let totaldoors = 3;
-
-    let state = "PICK";
-
-    let pickedDoor;
-
-    let autoMode = false;
-
-    let timeoutId;
-    
-    let statistics = {
-        totalSwitchPlays: 0,
-        totalStayPlays: 0,
-        totalSwitchWins: 0,
-        totalStayWins: 0,
-
-    };
 
 
-    
-        
     
